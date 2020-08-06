c6ec30421df24dd8fe61016ca4a535a35977cfd6
Matias <maticapuano97@gmail.com>
2020-08-06 18:36:45 -0500

    Added User me, for Example.

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/api/users/me", (req, res) => {
+  res.end("Hi there!");
+});
+
 app.listen(3000, () => {
   console.log(`Server ready on PORT 3000`);
 });
