d0f9f3745322d20f7c1e4a01a6fdb4a6bbab6600
Matias <maticapuano97@gmail.com>
2020-08-14 17:26:04 -0300

    Implemented signout.

--- auth/src/routes/signOut.ts
+++ auth/src/routes/signOut.ts
@@ -3,7 +3,9 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.post("/api/users/signout", (req: Request, res: Response) => {
-  res.end("Hi there!!");
+  req.session = null;
+
+  res.status(201).json({});
 });
 
 export { router as siginoutRouter };
