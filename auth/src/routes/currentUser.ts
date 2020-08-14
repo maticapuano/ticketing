0f844f6d25888012505a368f6be861c2dba74e8d
Matias <maticapuano97@gmail.com>
2020-08-14 17:21:31 -0300

    Implemented show current user signIn.

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,20 @@
 import { Router, Response, Request } from "express";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
 router.get("/api/users/me", (req: Request, res: Response) => {
-  res.end("Hi there!!");
+  if (!req.session?.jwt) {
+    return res.status(401).json({ data: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+
+    return res.status(200).json({ data: payload });
+  } catch (err) {
+    return res.status(401).json({ data: null });
+  }
 });
 
 export { router as currentUserRouter };
