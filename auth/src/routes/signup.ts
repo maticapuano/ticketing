3feafc539cf1871d35ba99a9ff18cc2ef3d548e3
Matias <maticapuano97@gmail.com>
2020-08-14 16:51:41 -0300

    Implemented validate request.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/User";
 import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = Router();
 
@@ -19,12 +20,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters."),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
     const { email, password } = req.body;
 
     const exitingUser = await User.findOne({ email });
