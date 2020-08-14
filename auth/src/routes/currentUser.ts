87bb2e84c9b12e7742eb0e94fd3fe4e4b9d83d75
Matias <maticapuano97@gmail.com>
2020-08-14 17:42:10 -0300

    Refactoring get data current user.

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,20 +1,11 @@
 import { Router, Response, Request } from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = Router();
 
-router.get("/api/users/me", (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.status(401).json({ data: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
-
-    return res.status(200).json({ data: payload });
-  } catch (err) {
-    return res.status(401).json({ data: null });
-  }
+router.get("/api/users/me", currentUser, (req: Request, res: Response) => {
+  return res.status(200).json({ data: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
