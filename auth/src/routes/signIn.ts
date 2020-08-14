1ac9eb1d91b1d3622f904ff0b28ac8acda216959
Matias <maticapuano97@gmail.com>
2020-08-14 17:03:45 -0300

    Implemented signIn.

--- auth/src/routes/signIn.ts
+++ auth/src/routes/signIn.ts
@@ -1,6 +1,10 @@
 import { Router, Response, Request } from "express";
 import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
+import jwt from "jsonwebtoken";
+import { User } from "../models/User";
+import { Password } from "../services/password";
 
 const router = Router();
 
@@ -13,14 +17,30 @@ router.post(
       .notEmpty()
       .withMessage("You must supply a password"),
   ],
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
+    const exitingUser = await User.findOne({ email });
+
+    if (!exitingUser) {
+      throw new BadRequestError("Invalid credentials");
     }
 
-    res.end("Hi there!!");
+    const passwordMath = await Password.compare(exitingUser.password, password);
+
+    if (!passwordMath) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const token = jwt.sign(
+      {
+        id: exitingUser.id,
+        email: exitingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+    return res.json({ data: exitingUser, token });
   }
 );
 
