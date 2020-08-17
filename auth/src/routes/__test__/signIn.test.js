8807cfdd4bd86f23ac8f9da21da7da0dd3b626ae
Matias <maticapuano97@gmail.com>
2020-08-17 19:37:18 -0300

    Test - incorrect password is supplied.

--- auth/src/routes/__test__/signIn.test.js
+++ auth/src/routes/__test__/signIn.test.js
@@ -10,3 +10,21 @@ it("fail's when a email that does not exits", async () => {
     })
     .expect(400);
 });
+
+it("fail's when a incorrect password is supplied", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "lalalalalalala",
+    })
+    .expect(400);
+});
