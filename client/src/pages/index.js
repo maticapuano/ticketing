438390621645a94f21d3bf3845296ddd00e56b3a
Matias <maticapuano97@gmail.com>
2020-08-20 13:28:34 -0300

    Implemented buildClient. get info user in signup.

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,15 @@
-const HelloWord = () => {
+import buildClient from "../api/build-client";
+
+const HelloWord = ({ data }) => {
+  console.log(data);
   return <h1>Hello word</h1>;
+};
+
+HelloWord.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/me");
+
+  return data;
 };
 
 export default HelloWord;
