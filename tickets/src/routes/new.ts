6b2a0a6e79c742f73f93b3df344dc598fa0d0197
Matias Capuano <maticapuano97@gmail.com>
2020-08-31 18:51:01 -0300

    Implemented validation for passed tests. & implemented validation.

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,22 @@
 import { Router, Request, Response } from "express";
-import { requireAuth } from "@mcticketing/common";
+import { requireAuth, validateRequest } from "@mcticketing/common";
+import { body } from "express-validator";
+
 const router = Router();
 
-router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
-  return res.status(200).json({});
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is requerid"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    return res.status(200).json({});
+  }
+);
 
 export { router as newTicketRouter };
