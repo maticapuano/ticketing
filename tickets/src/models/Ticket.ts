f0a7b100ca03c8069dbb98d2e00575b6820705c5
Matias Capuano <maticapuano97@gmail.com>
2020-09-01 16:14:59 -0300

    Added schema tickets.

--- tickets/src/models/Ticket.ts
+++ tickets/src/models/Ticket.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 interface TicketAttrs {
   title: string;
@@ -15,3 +15,38 @@ interface TicketDoc extends mongoose.Document {
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
+
+const ticketSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    price: {
+      type: Number,
+      min: 1,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
