d39aa14153016850ac5254109b070e50272f07b8
Matias Capuano <maticapuano97@gmail.com>
2020-09-02 12:48:22 -0300

    Return 200 if a ticket is exits by id.

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -8,3 +8,23 @@ it("Returns 404 if ticket is not found", async () => {
     .send()
     .expect(404);
 });
+
+it("Return 200 if a ticket is found", async () => {
+  const title = "ticket test";
+  const price = 25;
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title, price })
+    .expect(201);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.data.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(200);
+
+  expect(ticketResponse.body.data.title).toEqual(title);
+  expect(ticketResponse.body.data.price).toEqual(price);
+});
