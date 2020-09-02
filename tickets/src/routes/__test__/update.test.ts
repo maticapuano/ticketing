70e7d77fdc5f393b95391a3e4eac05484f61327c
Matias Capuano <maticapuano97@gmail.com>
2020-09-02 20:33:09 -0300

    Added test if an user attempt update ticket without auth.

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("Return a 401 if a user attempt update without first signed in.", async () => {
+  const title = "ticket test";
+  const price = 21;
+
+  //Request for create an ticket
+  const response = await request(app)
+    .post("/api/tickets")
+    .send("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title,
+      price,
+    })
+    .expect(201);
+
+  //Update a ticket
+  const titleUpdate = "ticket updated";
+  const priceUpdate = 95;
+  const ticket_id = response.body.data.id;
+
+  await request(app)
+    .put(`/api/tickets/${ticket_id}`)
+    .send({
+      titleUpdate,
+      priceUpdate,
+    })
+    .expect(401);
+});
