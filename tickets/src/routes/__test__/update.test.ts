afdabed8dce74b41c8b72f822399ff48df6ff95b
Matias Capuano <maticapuano97@gmail.com>
2020-09-03 12:15:12 -0300

    Return an 400 bad request if the user provider invalid title or price.

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -45,3 +45,45 @@ it("return an 401 if user does not own the ticket.", async () => {
     })
     .expect(401);
 });
+
+it("return an 400 bad request if the user provider invalid title or price.", async () => {
+  const cookie = global.signin();
+
+  //Create ticket
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "lalala",
+      price: 25,
+    })
+    .expect(201);
+
+  //Update ticket
+  await request(app)
+    .put(`/api/tickets/${response.body.data.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "",
+      price: -1000,
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.data.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "",
+      price: "100",
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.data.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "good title",
+      price: -10,
+    })
+    .expect(400);
+});
