e76c1a1247a3dd5581f15c6b38dc4170f8e40e79
Matias Capuano <maticapuano97@gmail.com>
2020-09-02 23:52:37 -0300

    added update ticket test, return an 404 if the user provider an id does not exits.

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,31 +1,9 @@
 import request from "supertest";
 import { app } from "../../app";
+import mongoose from "mongoose";
 
-it("Return a 401 if a user attempt update a ticket without first signed in.", async () => {
-  const title = "ticket test";
-  const price = 21;
+it("return an 404 if the provided id does not exits", async () => {
+  const id = mongoose.Types.ObjectId().toHexString();
 
-  //Request for create an ticket
-  const response = await request(app)
-    .post("/api/tickets")
-    .send("/api/tickets")
-    .set("Cookie", global.signin())
-    .send({
-      title,
-      price,
-    })
-    .expect(201);
-
-  //Update a ticket
-  const titleUpdate = "ticket updated";
-  const priceUpdate = 95;
-  const ticket_id = response.body.data.id;
-
-  await request(app)
-    .put(`/api/tickets/${ticket_id}`)
-    .send({
-      titleUpdate,
-      priceUpdate,
-    })
-    .expect(401);
+  await request(app).put(`/api/tickets/${id}`).expect(404);
 });
