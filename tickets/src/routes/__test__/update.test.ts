c7f708a61edf5568af6e636de78b934df92cd220
Matias Capuano <maticapuano97@gmail.com>
2020-09-02 23:57:46 -0300

    Fixed update test, added cookie session & send data.

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -5,5 +5,12 @@ import mongoose from "mongoose";
 it("return an 404 if the provided id does not exits", async () => {
   const id = mongoose.Types.ObjectId().toHexString();
 
-  await request(app).put(`/api/tickets/${id}`).expect(404);
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "lala",
+      price: 25,
+    })
+    .expect(404);
 });
