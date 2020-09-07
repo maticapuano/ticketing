b13bf926114e3749c4526c7244217d65edc75cdd
Matias Capuano <maticapuano97@gmail.com>
2020-09-07 16:43:06 -0300

    nats listeing in group.

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,19 @@
 import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
 console.clear();
 
-const stan = nats.connect("ticketing", "1234", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listen connected to NATS.");
 
-  const subscription = stan.subscribe("ticket:created");
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "order-service-queue-group"
+  );
 
   subscription.on("message", (message: Message) => {
     const data = message.getData();
