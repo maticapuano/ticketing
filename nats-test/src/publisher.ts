d92cc376790d34356ab53237428eb1f8d11806ab
Matias Capuano <maticapuano97@gmail.com>
2020-09-12 10:21:02 -0500

    Implemented async await.

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,12 +7,12 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS.");
 
   const publisher = new TicketCreatedPublisher(stan);
 
-  publisher.publish({
+  await publisher.publish({
     id: "1b112ac2-0960-408f-bd24-97e2bc0b7a65",
     price: 12,
     title: "lalala",
