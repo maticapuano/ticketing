271ed6ce41175f1a7706f4f325ef4af481c02749
Matias Capuano <maticapuano97@gmail.com>
2020-09-18 01:22:06 -0300

    Implemented env NATS.

--- tickets/src/server.ts
+++ tickets/src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 import mongoose from "mongoose";
-import { natsWrapper } from "./events/nats-wrapper";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -9,12 +9,21 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must defined");
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must defined");
+  }
 
   try {
     await natsWrapper.connect(
-      "ticketing",
-      "tickets-service",
-      "http://nats-srv:4222"
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
     );
 
     //Close all connections exiting
