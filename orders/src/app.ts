4d85c7f70fa06cba2a28658d110792c95227fdd4
Matias Capuano <maticapuano97@gmail.com>
2020-09-20 14:54:53 -0300

    Added app for startup express server.

---
+++ orders/src/app.ts
@@ -0,0 +1,28 @@
+import express, { NextFunction, Response, Request } from "express";
+import cookieSession from "cookie-session";
+import "express-async-errors";
+
+import { errorHandler, NotFoundError, currentUser } from "@mcticketing/common";
+import { indexTestRouter } from "./routes";
+
+const app = express();
+
+app.set("trust proxy", 1);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+
+app.use(currentUser);
+
+app.use(indexTestRouter);
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  return next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
+export { app };
