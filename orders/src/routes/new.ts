be94d84df3eaa67c9399826caf0c5d2829ddb40c
Matias Capuano <maticapuano97@gmail.com>
2020-09-21 23:30:42 -0300

    Added validation before new order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,11 +1,26 @@
 import { Request, Response, Router } from "express";
+import { requireAuth, validateRequest } from "@mcticketing/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "The service the orders Working method post 😁😁😜😍",
-  });
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provider."),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.status(200).json({
+      message: "The service the orders Working method post 😁😁😜😍",
+    });
+  }
+);
 
 export { router as newOrderRouter };
