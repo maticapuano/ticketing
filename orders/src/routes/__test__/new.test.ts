84b50576ce320ed2bbca81c9c819621b5964acf4
Matias Capuano <maticapuano97@gmail.com>
2020-09-27 16:24:38 -0300

    Return an error if the ticket is reserved.

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,9 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
+import { Ticket } from "../../models/ticket";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@mcticketing/common";
 
 it("Return an error if the ticket does not exits", async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -18,4 +21,32 @@ it("Return an 401 unauthorized if not provided cookie at token", async () => {
   const response = await request(app).post("/api/orders").send();
 
   expect(response.status).toEqual(401);
+});
+
+it("Returns an error if the ticket is already reserved.", async () => {
+  const ticket = Ticket.build({
+    title: "Foo ticket concert",
+    price: 250,
+  });
+
+  await ticket.save();
+
+  //Create order of the ticket
+  const order = Order.build({
+    ticket: ticket.id,
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+    userId: mongoose.Types.ObjectId().toHexString(),
+  });
+
+  await order.save();
+
+  //Expected after bad request
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(400);
 });
