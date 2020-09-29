57f46c29b05478114f1f6d30b979fed33dc081c7
Matias Capuano <maticapuano97@gmail.com>
2020-09-29 00:40:32 -0300

    Implemented, delete a order by id.

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,11 +1,39 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+} from "@mcticketing/common";
 import { Request, Response, Router } from "express";
+import { Order } from "../models/order";
 
 const router = Router();
 
-router.delete("/api/orders/:id", async (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "The service the orders Working method DELETE 😁😁😜😍",
-  });
-});
+router.delete(
+  "/api/orders/:id",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const order = await Order.findById(id);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser?.id) {
+      throw new NotAuthorizedError();
+    }
+
+    //Update status order to Canceled
+
+    order.status = OrderStatus.Canceled;
+
+    await order.save();
+
+    return res.status(202).json({
+      data: order,
+    });
+  }
+);
 
 export { router as deleteOrderRouter };
