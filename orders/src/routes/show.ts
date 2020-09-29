04953bdbfd7f35bcb639f4b2882da70a7d467b85
Matias Capuano <maticapuano97@gmail.com>
2020-09-28 21:55:01 -0300

    Show a order by id implemented. & only show order if the user belong at order.

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,11 +1,31 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@mcticketing/common";
 import { Request, Response, Router } from "express";
+import { Order } from "../models/order";
 
 const router = Router();
 
-router.get("/api/orders/:id", async (req: Request, res: Response) => {
-  res.status(200).json({
-    message: `The service the orders Working method GET ${req.params.id} 😁😁😜😍`,
-  });
-});
+router.get(
+  "/api/orders/:id",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.id).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser?.id) {
+      throw new NotAuthorizedError();
+    }
+
+    return res.status(200).json({
+      data: order,
+    });
+  }
+);
 
 export { router as showOrderRouter };
