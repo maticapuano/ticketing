38bb5d3649ad44dac2431d93aed32b3372c97183
Matias Capuano <maticapuano97@gmail.com>
2020-09-30 00:57:18 -0300

    Emits a event order cancelled event.

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import { Ticket } from "../../models/ticket";
 import { OrderStatus } from "@mcticketing/common";
 import { Order } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("Return a 401 unauthorized if not at signin", async () => {
   const id = mongoose.Types.ObjectId();
@@ -71,4 +72,34 @@ it("Return an error if other user tries to delete order other user.", async () =
   expect(cancelOrderOtherUser.status).toEqual(401);
 });
 
-it.todo("Emits a event order canceled event.");
+it("Emits a event order canceled event.", async () => {
+  const ticket = Ticket.build({ title: "Concert pop!!", price: 256.32 });
+  await ticket.save();
+
+  //Create User
+  const user = global.signin();
+
+  //Create a Order.
+  const order = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  //Delete a Order
+  const id = order.body.order.id;
+  const orderCancel = await request(app)
+    .delete(`/api/orders/${id}`)
+    .set("Cookie", user)
+    .send();
+
+  expect(orderCancel.status).toEqual(202);
+
+  //Check if at order if mark at canceled
+  const updatedOrder = await Order.findById(id);
+
+  expect(updatedOrder?.status).toEqual(OrderStatus.Canceled);
+
+  //Expected called method publish
+  expect(natsWrapper.getClient.publish).toHaveBeenCalled();
+});
