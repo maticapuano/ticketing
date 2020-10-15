d6e24b0606b5145c3af3d4854231e97d51fe6ff0
Matias Capuano <maticapuano97@gmail.com>
2020-10-14 21:12:30 -0500

    Added version number.

--- tickets/src/models/Ticket.ts
+++ tickets/src/models/Ticket.ts
@@ -11,7 +11,7 @@ interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
   userId: string;
-  version:number
+  version: number;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -45,9 +45,9 @@ const ticketSchema = new Schema(
   }
 );
 
-ticketSchema.set('versionKey','version')
+ticketSchema.set("versionKey", "version");
 
-ticketSchema.plugin(updateIfCurrentPlugin)
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
