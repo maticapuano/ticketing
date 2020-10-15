ff2728bd952947338805f9e9fbaafc796a9ca626
Matias Capuano <maticapuano97@gmail.com>
2020-10-14 22:03:35 -0500

    Added current version mongoose.

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { OrderStatus } from "@mcticketing/common";
 import { TicketDoc } from "./ticket";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface OrderAttrs {
   userId: string;
@@ -14,6 +15,7 @@ interface OrderDocument extends mongoose.Document {
   status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
+  version: number;
 }
 
 interface OrderModel extends mongoose.Model<OrderDocument> {
@@ -50,6 +52,10 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set("versionKey", "version");
+
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
