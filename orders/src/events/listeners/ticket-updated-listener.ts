6c8eaedfe5adf5453b3a2f1b7b9c0b51d43c9d58
Matias Capuano <maticapuano97@gmail.com>
2020-10-15 22:12:46 -0500

    Fixed version event.

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,9 +8,9 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-    const { id, title, price } = data;
+    const { title, price } = data;
     const ticket = await Ticket.findOne({
-      _id: id,
+      _id: data.id,
       version: data.version - 1,
     });
 
