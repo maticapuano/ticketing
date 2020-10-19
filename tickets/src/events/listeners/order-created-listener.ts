0e94bac0a1fb0ff2f9b829f287bf766de109c458
Matías Capuano <maticapuano97@gmail.com>
2020-10-19 14:38:49 -0500

    Implemented, mark ticket a reserverd a order is created.

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,10 +1,28 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@mcticketing/common";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/Ticket";
 import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName: string = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If not ticket , throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    //Save the ticket
+    await ticket.save();
+
+    // Ack the message.
+    msg.ack();
+  }
 }
