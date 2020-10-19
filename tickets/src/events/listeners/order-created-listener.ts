9c65794b0016096e58e24a9b678bffe794364285
Matías Capuano <maticapuano97@gmail.com>
2020-10-19 14:25:04 -0500

    Added order created listener.

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,10 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@mcticketing/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
