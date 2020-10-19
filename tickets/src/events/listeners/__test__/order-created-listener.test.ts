41d92869f150184602235b358e003a2644267562
Matías Capuano <maticapuano97@gmail.com>
2020-10-19 14:49:33 -0500

    Implemented setup fake data.

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,39 @@
+import { OrderCreatedEvent, OrderStatus } from "@mcticketing/common";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/Ticket";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCreatedListener } from "../order-created-listener";
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.getClient);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    title: "foo",
+    price: 12.9,
+    userId: mongoose.Types.ObjectId().toHexString(),
+  });
+
+  await ticket.save();
+
+  //Create the fake data for event
+  const data: OrderCreatedEvent["data"] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    userId: mongoose.Types.ObjectId().toHexString(),
+    expiresAt: new Date().toISOString(),
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+    version: 0,
+  };
+
+  //@ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg, ticket };
+};
