1bc656e296a1f6d73cd2f73661f15d87f18333a9
Matías Capuano <maticapuano97@gmail.com>
2020-10-26 12:52:56 -0600

    Implemented Expiration Queue on produce a event.

--- expiration/src/events/listeners/order-created-listeners.ts
+++ expiration/src/events/listeners/order-created-listeners.ts
@@ -1,10 +1,23 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@mcticketing/common";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 import { queueGroupName } from "./queueGroupName";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const { id: orderId } = data;
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      {
+        orderId,
+      },
+      { delay }
+    );
+
+    msg.ack();
+  }
 }
