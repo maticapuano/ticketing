9150da75a9f2ca0c215cbdf5ef4b48e871aa4720
Matías Capuano <maticapuano97@gmail.com>
2020-10-27 14:45:30 -0600

    Fix route test.

--- payments/src/routes/index.ts
+++ payments/src/routes/index.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from "express";
 
 const router = Router();
 
-router.get("/api/payment", (req: Request, res: Response) => {
+router.get("/api/payments", (req: Request, res: Response) => {
   return res.json({
     service: "payments",
     work: true,
