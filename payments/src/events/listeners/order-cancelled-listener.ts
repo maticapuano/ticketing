1676b363f765785830dc579980b0b47ea41f50e4
Matías Capuano <maticapuano97@gmail.com>
2020-10-27 20:58:13 -0600

    Order cancelled event listener.

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,32 @@
+import {
+  Listener,
+  OrderCancelledEvent,
+  OrderStatus,
+  Subjects,
+} from "@mcticketing/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  queueGroupName = queueGroupName;
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    const order = await Order.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    });
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    order.set({ status: OrderStatus.Canceled });
+
+    await order.save();
+
+    //Ack message
+    msg.ack();
+  }
+}
