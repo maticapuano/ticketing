6a7e2bbd256263d769891fd880768aae10e8fb91
Matías Capuano <maticapuano97@gmail.com>
2020-10-27 21:27:57 -0600

    Fix bug increment version number.

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,5 +1,6 @@
 import { OrderStatus } from "@mcticketing/common";
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface OrderAttrs {
   id: string;
@@ -48,14 +49,7 @@ const orderSchema = new mongoose.Schema(
 
 orderSchema.set("versionKey", "version");
 
-orderSchema.pre("save", function (done) {
-  // @ts-ignore
-  this.$where = {
-    version: this.get("version") - 1,
-  };
-
-  done();
-});
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
