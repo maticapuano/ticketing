e3772172d8351ce6d2ea1ef4a16b5c87a77410d2
Matías Capuano <maticapuano97@gmail.com>
2020-10-28 20:19:37 -0600

    Return a 401 when pushing an order that doesn't belong the user

--- payments/src/routes/__tests__/new.test.ts
+++ payments/src/routes/__tests__/new.test.ts
@@ -1,6 +1,8 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@mcticketing/common";
 
 it("Returns a 404 when punching an order that does not exits", async () => {
   await request(app)
@@ -11,4 +13,26 @@ it("Returns a 404 when punching an order that does not exits", async () => {
       orderId: mongoose.Types.ObjectId().toHexString(),
     })
     .expect(404);
+});
+
+it("Return a 401 when pushing an order that doesn't belong the user", async () => {
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    price: 9.99,
+    status: OrderStatus.Created,
+    userId: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+  });
+
+  await order.save();
+
+  const response = await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "foo12345",
+      orderId: order.id,
+    });
+
+  expect(response.status).toEqual(401);
 });
