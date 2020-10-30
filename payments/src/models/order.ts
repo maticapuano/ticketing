1d123556f8761c6fd19756df13063aefdd9d8b16
Matías Capuano <maticapuano97@gmail.com>
2020-10-29 19:32:33 -0600

    Fix bug not show id.

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -40,7 +40,7 @@ const orderSchema = new mongoose.Schema(
   {
     toJSON: {
       transform(doc, ret) {
-        ret._id = ret.id;
+        ret.id = ret._id;
         delete ret._id;
       },
     },
