7db1cdcb1a83b1723a2d9e4f1ce99c9b658e4e81
Matías Capuano <maticapuano97@gmail.com>
2020-11-01 15:29:35 -0600

    Added new ticket page.

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,11 +4,8 @@ const HelloWord = ({ data }) => {
   return data ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
 };
 
-HelloWord.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get("/api/users/me");
-
-  return data;
+HelloWord.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default HelloWord;
