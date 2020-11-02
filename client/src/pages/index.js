9f5759d6457277649a723d41590ce7dbb4084b23
Matías Capuano <maticapuano97@gmail.com>
2020-11-01 19:33:27 -0600

    Implemented ticket list.

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,33 @@
-import buildClient from "../api/build-client";
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
 
-const HelloWord = ({ data }) => {
-  return data ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
 };
 
-HelloWord.getInitialProps = async (context, client, currentUser) => {
-  return {};
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get("/api/tickets");
+
+  return { tickets: data.data };
 };
 
-export default HelloWord;
+export default LandingPage;
