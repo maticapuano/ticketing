1eb1c12f9db55bef85f6085b5fe9202f50371170
Matías Capuano <maticapuano97@gmail.com>
2020-11-02 10:34:42 -0600

    List all orders page.

---
+++ client/src/pages/orders/index.js
@@ -0,0 +1,21 @@
+const OrderIndex = ({ orders }) => {
+  return (
+    <ul>
+      {orders.map((order) => {
+        return (
+          <li key={order.id}>
+            {order.ticket.title} - <b>{order.status}</b>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+OrderIndex.getInitialProps = async (context, client) => {
+  const { data } = await client.get("/api/orders");
+
+  return { orders: data.data };
+};
+
+export default OrderIndex;
